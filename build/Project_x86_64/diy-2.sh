7f1850b2038a741ca5d944845e0336cb7c1b56b1
danshui-git <68673669+danshui-git@users.noreply.github.com>
2021-05-21 00:23:11 +0800

    Update diy-2.sh

--- build/Project_x86_64/diy-2.sh
+++ build/Project_x86_64/diy-2.sh
@@ -18,9 +18,9 @@ echo "281677160 Compiled in $(TZ=UTC-8 date "+%Y.%m.%d")" > package/base-files/f
 
 sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'" $ZZZ                       # 修改主机名称为OpenWrt-123
 
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$PhflQnJ1$yamWfH5Mphs4hXV7UXWQ21:18725/g' $ZZZ           # 替换密码（要替换密码就不能设置密码为空）
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$PhflQnJ1$yamWfH5Mphs4hXV7UXWQ21:18725/g' $ZZZ          # 替换密码（要替换密码就不能设置密码为空）
 
-#sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                                   # 设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                                    # 设置密码为空
 
 #sed -i 's/PATCHVER:=5.4/PATCHVER:=4.19/g' target/linux/x86/Makefile                                      # 修改内核版本为4.19
 
